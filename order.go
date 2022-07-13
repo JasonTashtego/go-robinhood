d90548a01c6e61a168230c75516640fccb5f5c4c
Jason DeBettencourt <jason@tashtegollc.com>
2022-07-13 15:58:45 -0400

    fix type for ipo_access_execution_rank

--- order.go
+++ order.go
@@ -43,62 +43,59 @@ const (
 )
 
 const (
-	StopTrigger  = "stop"
-	ImmTrigger = "immediate"
+	StopTrigger = "stop"
+	ImmTrigger  = "immediate"
 
 	TrailTypePrice = "price"
 )
 
-
-
 type TrailPeg struct {
 	Price struct {
 		Amount       float64 `json:"amount,string,omitempty"`
-		CurrencyCode string `json:"currency_code,omitempty"`
+		CurrencyCode string  `json:"currency_code,omitempty"`
 	} `json:"price,omitempty"`
 	Type string `json:"type,omitempty"`
 }
 
 type RhOrder struct {
-	Account       string  `json:"account,omitempty"`
-	ExtendedHours bool    `json:"extended_hours"`
-	Instrument    string  `json:"instrument,omitempty"`
-	Price         float64 `json:"price,string,omitempty"`
-	Quantity      float64 `json:"quantity,string,omitempty"`
-	RefID         string  `json:"ref_id,omitempty"`
-	Side          string  `json:"side,omitempty"`
-	Symbol        string  `json:"symbol,omitempty"`
-	TimeInForce   string  `json:"time_in_force,omitempty"`
-	Trigger       string  `json:"trigger,omitempty"`
-	Type          string  `json:"type,omitempty"`
-	StopPrice     float64  `json:"stop_price,string,omitempty"`
-	TrailingPeg * TrailPeg `json:"trailing_peg,omitempty"`
+	Account       string    `json:"account,omitempty"`
+	ExtendedHours bool      `json:"extended_hours"`
+	Instrument    string    `json:"instrument,omitempty"`
+	Price         float64   `json:"price,string,omitempty"`
+	Quantity      float64   `json:"quantity,string,omitempty"`
+	RefID         string    `json:"ref_id,omitempty"`
+	Side          string    `json:"side,omitempty"`
+	Symbol        string    `json:"symbol,omitempty"`
+	TimeInForce   string    `json:"time_in_force,omitempty"`
+	Trigger       string    `json:"trigger,omitempty"`
+	Type          string    `json:"type,omitempty"`
+	StopPrice     float64   `json:"stop_price,string,omitempty"`
+	TrailingPeg   *TrailPeg `json:"trailing_peg,omitempty"`
 
 	OverrideDayTradeChecks bool `json:"override_day_trade_checks,omitempty"`
 	OverrideDtbpChecks     bool `json:"override_dtbp_checks,omitempty"`
 }
 
-func (c * Client) CreateOrder(i * Instrument) * RhOrder {
+func (c *Client) CreateOrder(i *Instrument) *RhOrder {
 
 	newOrd := RhOrder{
-		Account:       c.Account.URL,
-		Symbol:        i.Symbol,
-		Instrument:    i.URL,
-		TimeInForce:   strings.ToLower(GTC.String()),
-		Type:          strings.ToLower(Market.String()),
-		Trigger: 	   ImmTrigger,
+		Account:     c.Account.URL,
+		Symbol:      i.Symbol,
+		Instrument:  i.URL,
+		TimeInForce: strings.ToLower(GTC.String()),
+		Type:        strings.ToLower(Market.String()),
+		Trigger:     ImmTrigger,
 	}
 	return &newOrd
 }
 
-
 // Order places an order for a given instrument. Cancellation of the given
 // context cancels only the _http request_ and not any orders that may have
 // been created regardless of the cancellation.
-func (c *Client) SubmitOrder(ctx context.Context, rhOrd * RhOrder) (*OrderOutput, error) {
+func (c *Client) SubmitOrder(ctx context.Context, rhOrd *RhOrder) (*OrderOutput, error) {
 
 	// no fractional pennies.
-	usdPrice := int(rhOrd.Price*100)
+	usdPrice := int(rhOrd.Price * 100)
 	rhOrd.Price = float64(usdPrice) / 100.0
 	rhOrd.Side = strings.ToLower(rhOrd.Side)
 	rhOrd.Type = strings.ToLower(rhOrd.Type)
@@ -128,84 +125,81 @@ func (c *Client) SubmitOrder(ctx context.Context, rhOrd * RhOrder) (*OrderOutput
 
 // OrderOutput is the response from the Order api
 type OrderOutput struct {
-
-	ID                 string      `json:"id"`
-	RefID              string      `json:"ref_id"`
-	URL                string      `json:"url"`
-	Account            string      `json:"account"`
-	Position           string      `json:"position"`
-	CancelURL          string	   `json:"cancel"`
-	Instrument         string      `json:"instrument"`
-	CumulativeQuantity float64     `json:"cumulative_quantity,string"`
-	AveragePrice       float64     `json:"average_price,string"`
-	Fees               float64     `json:"fees,string"`
-	State              string      `json:"state"`
-	Type               string      `json:"type"`
-	Side               string      `json:"side"`
-	TimeInForce        string      `json:"time_in_force"`
-	Trigger            string      `json:"trigger"`
-	Price              float64     `json:"price,string"`
-	StopPrice          float64 	   `json:"stop_price,string"`
-	Quantity           float64     `json:"quantity,string"`
-	RejectReason       string      `json:"reject_reason"`
-	CreatedAt          time.Time   `json:"created_at"`
-	UpdatedAt          time.Time   `json:"updated_at"`
-	LastTransactionAt  time.Time   `json:"last_transaction_at"`
+	ID                 string    `json:"id"`
+	RefID              string    `json:"ref_id"`
+	URL                string    `json:"url"`
+	Account            string    `json:"account"`
+	Position           string    `json:"position"`
+	CancelURL          string    `json:"cancel"`
+	Instrument         string    `json:"instrument"`
+	CumulativeQuantity float64   `json:"cumulative_quantity,string"`
+	AveragePrice       float64   `json:"average_price,string"`
+	Fees               float64   `json:"fees,string"`
+	State              string    `json:"state"`
+	Type               string    `json:"type"`
+	Side               string    `json:"side"`
+	TimeInForce        string    `json:"time_in_force"`
+	Trigger            string    `json:"trigger"`
+	Price              float64   `json:"price,string"`
+	StopPrice          float64   `json:"stop_price,string"`
+	Quantity           float64   `json:"quantity,string"`
+	RejectReason       string    `json:"reject_reason"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
+	LastTransactionAt  time.Time `json:"last_transaction_at"`
 	Executions         []struct {
-		Price                  float64     `json:"price,string"`
-		Quantity               float64     `json:"quantity,string"`
-		SettlementDate         string      `json:"settlement_date"`
-		Timestamp              time.Time   `json:"timestamp"`
-		ID                     string      `json:"id"`
-		IpoAccessExecutionRank string      `json:"ipo_access_execution_rank"`
+		Price                  float64   `json:"price,string"`
+		Quantity               float64   `json:"quantity,string"`
+		SettlementDate         string    `json:"settlement_date"`
+		Timestamp              time.Time `json:"timestamp"`
+		ID                     string    `json:"id"`
+		IpoAccessExecutionRank float64   `json:"ipo_access_execution_rank"`
 	} `json:"executions"`
-	ExtendedHours           bool        `json:"extended_hours"`
-	OverrideDtbpChecks      bool        `json:"override_dtbp_checks"`
-	OverrideDayTradeChecks  bool        `json:"override_day_trade_checks"`
-	ResponseCategory        string      `json:"response_category"`
-	StopTriggeredAt         time.Time   `json:"stop_triggered_at"`
+	ExtendedHours          bool      `json:"extended_hours"`
+	OverrideDtbpChecks     bool      `json:"override_dtbp_checks"`
+	OverrideDayTradeChecks bool      `json:"override_day_trade_checks"`
+	ResponseCategory       string    `json:"response_category"`
+	StopTriggeredAt        time.Time `json:"stop_triggered_at"`
 	TrailingPeg            struct {
 		Type       string `json:"type"`
 		Percentage int    `json:"percentage"`
-		Price struct {
+		Price      struct {
 			Amount       float64 `json:"amount,string,omitempty"`
-			CurrencyCode string `json:"currency_code,omitempty"`
+			CurrencyCode string  `json:"currency_code,omitempty"`
 		} `json:"price,omitempty"`
 	} `json:"trailing_peg"`
-	LastTrailPrice  struct {
+	LastTrailPrice struct {
 		Amount       float64 `json:"amount,string"`
-		CurrencyCode string `json:"currency_code"`
-		CurrencyID   string `json:"currency_id"`
+		CurrencyCode string  `json:"currency_code"`
+		CurrencyID   string  `json:"currency_id"`
 	} `json:"last_trail_price"`
-	LastTrailPriceUpdatedAt time.Time   `json:"last_trail_price_updated_at"`
-	DollarBasedAmount  struct {
+	LastTrailPriceUpdatedAt time.Time `json:"last_trail_price_updated_at"`
+	DollarBasedAmount       struct {
 		Amount       float64 `json:"amount,string"`
-		CurrencyCode string `json:"currency_code"`
-		CurrencyID   string `json:"currency_id"`
-	} 	`json:"dollar_based_amount"`
-	TotalNotional           struct {
+		CurrencyCode string  `json:"currency_code"`
+		CurrencyID   string  `json:"currency_id"`
+	} `json:"dollar_based_amount"`
+	TotalNotional struct {
 		Amount       float64 `json:"amount,string"`
-		CurrencyCode string `json:"currency_code"`
-		CurrencyID   string `json:"currency_id"`
+		CurrencyCode string  `json:"currency_code"`
+		CurrencyID   string  `json:"currency_id"`
 	} `json:"total_notional"`
 	ExecutedNotional struct {
 		Amount       float64 `json:"amount,string"`
-		CurrencyCode string `json:"currency_code"`
-		CurrencyID   string `json:"currency_id"`
+		CurrencyCode string  `json:"currency_code"`
+		CurrencyID   string  `json:"currency_id"`
 	} `json:"executed_notional"`
-	InvestmentScheduleID        string 		`json:"investment_schedule_id"`
-	IsIpoAccessOrder            bool        `json:"is_ipo_access_order"`
-	IpoAccessCancellationReason string	 	`json:"ipo_access_cancellation_reason"`
-	IpoAccessLowerCollaredPrice float64 	`json:"ipo_access_lower_collared_price,string"`
-	IpoAccessUpperCollaredPrice float64 	`json:"ipo_access_upper_collared_price,string"`
-	IpoAccessUpperPrice         float64 	`json:"ipo_access_upper_price,string"`
-	IpoAccessLowerPrice         float64	 	`json:"ipo_access_lower_price,string"`
-	IsIpoAccessPriceFinalized   bool        `json:"is_ipo_access_price_finalized"`
+	InvestmentScheduleID        string  `json:"investment_schedule_id"`
+	IsIpoAccessOrder            bool    `json:"is_ipo_access_order"`
+	IpoAccessCancellationReason string  `json:"ipo_access_cancellation_reason"`
+	IpoAccessLowerCollaredPrice float64 `json:"ipo_access_lower_collared_price,string"`
+	IpoAccessUpperCollaredPrice float64 `json:"ipo_access_upper_collared_price,string"`
+	IpoAccessUpperPrice         float64 `json:"ipo_access_upper_price,string"`
+	IpoAccessLowerPrice         float64 `json:"ipo_access_lower_price,string"`
+	IsIpoAccessPriceFinalized   bool    `json:"is_ipo_access_price_finalized"`
 
 	client *Client
 }
-
-
 
 // Update returns any errors and updates the item with any recent changes.
 func (o *OrderOutput) Update(ctx context.Context) error {
@@ -231,7 +225,7 @@ func (o OrderOutput) Cancel(ctx context.Context) error {
 	return nil
 }
 
-func (c * Client) CancelOrderById(ctx context.Context, id string) error {
+func (c *Client) CancelOrderById(ctx context.Context, id string) error {
 
 	var ordUrl = EPOrders + id + "/cancel/"
 
@@ -269,15 +263,15 @@ func (c *Client) RecentOrders(ctx context.Context) ([]OrderOutput, error) {
 	return o.Results, nil
 }
 
-func (c *Client) GetOrders(ctx context.Context, nextUrl * string, pgSize int64) ([]OrderOutput, string, error) {
+func (c *Client) GetOrders(ctx context.Context, nextUrl *string, pgSize int64) ([]OrderOutput, string, error) {
 	var o struct {
 		Results []OrderOutput
-		Next string
+		Next    string
 	}
 
 	url := EPOrders
 	if pgSize != 0 {
-		url = url + fmt.Sprintf("?page_size=%d",pgSize)
+		url = url + fmt.Sprintf("?page_size=%d", pgSize)
 	}
 	if nextUrl != nil {
 		url = *nextUrl
